e6746d13ac4eb49ea39f6b1000ce72204a2e111a
gina <watanabegina26@gmail.com>
2016-07-23 11:42:24 -1000

    Added for loop to display the ids of the buttons on click.
    Took out individual onclick functions for each button to display in text
    section.

--- js/register.js
+++ js/register.js
@@ -5,75 +5,13 @@ window.onload = function() {
   var clear = document.getElementById('clear');
   var newArray = [];
 
-button1.onclick = function() {
-  newArray.push(1);
-  display.innerHTML = newArray;
-}
-
-button2.onclick = function() {
-  newArray.push(2);
-  display.innerHTML = newArray;
-}
-
-button3.onclick = function() {
-  newArray.push(3);
-  display.innerHTML = newArray;
-}
-
-button4.onclick = function() {
-  newArray.push(4);
-  display.innerHTML = newArray;
-}
-
-button5.onclick = function() {
-  newArray.push(5);
-  display.innerHTML = newArray;
-}
-
-button6.onclick = function() {
-  newArray.push(6);
-  display.innerHTML = newArray;
-}
-
-button7.onclick = function() {
-  newArray.push(7);
-  display.innerHTML = newArray;
-}
-
-button8.onclick = function() {
-  newArray.push(8);
-  display.innerHTML = newArray;
-}
-
-button9.onclick = function() {
-  newArray.push(9);
-  display.innerHTML = newArray;
-}
-
-button0.onclick = function() {
-  newArray.push(0);
-  display.innerHTML = newArray;
-}
-
-plus.onclick = function() {
-   newArray.push("+");
-   display.innerHTML = newArray;
- }
-
- minus.onclick = function() {
-   newArray.push("-");
-   display.innerHTML = newArray;
- }
-
- multiply.onclick = function() {
-   newArray.push("x");
-   display.innerHTML = newArray;
- }
-
- divide.onclick = function() {
-   newArray.push("/");
-   display.innerHTML = newArray;
- }
+  for (i=0; i<button.length; i++) {
+    button[i].onclick = function() {
+      newArray.push(this.id);
+      display.innerHTML = newArray;
+      console.log(newArray);
+    }
+  }
 
  clear.onclick = function() {
   newArray.length = 0;

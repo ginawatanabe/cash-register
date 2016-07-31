c2507b42883a92e1fa902c75f5a23afd8046ccf1
gina <watanabegina26@gmail.com>
2016-07-30 14:30:27 -1000

    Added startmemory2 function.
    This will start the numbers after the operator and store them for later.

--- js/register.js
+++ js/register.js
@@ -12,21 +12,37 @@ window.onload = function() {
   var memory1 = '';
   var memory2 = '';
 
-  function startmemory1() {
-    for (i=0; i<button.length; i++) {
-        button[i].onclick = function() {
-        if (num1.innerHTML === '') {
-          memory1 = '';
-          memory1 = this.id;
-          num1.innerHTML = memory1;
-      } else if (num1.innerHTML !== '') {
-          num2.innerHTML = this.id;
-          memory1 = num1.innerHTML.concat(num2.innerHTML);
-          num1.innerHTML = memory1;
-          console.log(memory1);
-      }
-     }
+function startmemory1() {
+  for (i=0; i<button.length; i++) {
+      button[i].onclick = function() {
+      if (num1.innerHTML === '') {
+        memory1 = '';
+        memory1 = this.id;
+        num1.innerHTML = memory1;
+    } else if (num1.innerHTML !== '') {
+        num2.innerHTML = this.id;
+        memory1 = num1.innerHTML.concat(num2.innerHTML);
+        num1.innerHTML = memory1;
+        console.log(memory1);
     }
    }
+  }
+ }
 startmemory1();
+function startmemory2() {
+  for (i=0; i<button.length; i++) {
+      button[i].onclick = function() {
+      if (num4.innerHTML === '') {
+        memory2 = '';
+        memory2 = this.id;
+        num4.innerHTML = memory2;
+    } else if (num4.innerHTML !== '') {
+        num5.innerHTML = this.id;
+        memory2 = num4.innerHTML.concat(num5.innerHTML);
+        num4.innerHTML = memory2;
+        console.log(memory2);
+    }
+   }
+  }
+ }
 }
